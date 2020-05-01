b545809f1d2292203e55f47e67303fd7af332447
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-05-01 16:44:47 -0300

    Migrate about elements in about app

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from portfolio import urls
+from about import urls
 
 def home(request):
     return render(request, 'core/home.html')
 
-def about(request):
-    return render(request, 'core/about.html')
+
