a9948ea95e38e916226d52941dfa9d0b3ec26107
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-05-03 04:13:57 -0300

    Add favicon, change docs theme and add too blank in cv

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ik$x5=07j*id0_cfy5y_bkf^hp)kgof%3o3273wn1n##nu_g2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'http://everdev.pythonanywhere.com/']
 
