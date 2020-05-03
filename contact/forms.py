a4e4e3e5ec30a3ed6a857f80d51ce4b994556c91
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-05-03 04:23:49 -0300

    Change the language

--- contact/forms.py
+++ contact/forms.py
@@ -4,7 +4,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Escribe tu nombre'
+            'placeholder':'Please write your name'
         }
     ), min_length=2, max_length=100)
 
@@ -12,14 +12,14 @@ class ContactForm(forms.Form):
     email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Escribe tu email'
+            'placeholder':'Please write your email'
         }
     ), min_length=2, max_length=100)
 
     content = forms.CharField(label="Contenido", required=True, widget=forms.Textarea(
         attrs={
             'class':'form-control',
-            'placeholder':'Escribe tu mensaje por favor',
+            'placeholder':'Please write your message',
             'rows':5
         }
     ), min_length=2, max_length=100)
