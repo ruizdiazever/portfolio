080d4db8cd13ffcd7e7acdf47251c2914ca802bc
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-07-03 01:13:16 -0300

    New design for blog

--- core/models.py
+++ core/models.py
@@ -3,12 +3,14 @@ from django.db.models import FloatField
 
 
 class Blog(models.Model):
+    title = models.CharField(verbose_name="Titulo", blank=True, max_length=150)
+    subtitle = models.CharField(verbose_name="Subtitulo", blank=True, max_length=150, default="Nea")
     year = models.CharField(max_length=4, verbose_name="Año", null=True, blank=True)
-    datetime = models.DateTimeField(verbose_name="Datetime", auto_now=False, blank=True)
     text_time = models.CharField(max_length=6, verbose_name="Fecha")
-    title = models.TextField(verbose_name="Titulo", blank=True)
-    link = models.URLField(null=True, blank=True)
+    description = models.TextField(verbose_name="Descripcion", blank=True, null=True)
+    #datetime = models.DateTimeField(verbose_name="Datetime", auto_now=False, blank=True)
     ordering = FloatField(verbose_name='Orden', default=0)
+    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
 
