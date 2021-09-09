5b76fd35e12c214fe1ea6c4dfb05413ebb409274
Ever <ruizdiaz.oe@gmail.com>
2021-09-09 21:39:45 +0200

    Add dates of q1 begin and end days, december too

--- apps/steamdeck/static/steamdeck/time.js
+++ apps/steamdeck/static/steamdeck/time.js
@@ -8,8 +8,12 @@ var x = setInterval(function () {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("days").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("days").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";
+
+    var decEnd = (days + 31);
+    var q1_begin = (days + 31);
+    var q1_end = (q1_begin + 90);    
+    document.getElementById("time").innerHTML = "Dec " + days + "-" + decEnd + " days | Q1 " + q1_begin + "-" + q1_end + " days";
 
     if (distance < 0) {
         clearInterval(x);
